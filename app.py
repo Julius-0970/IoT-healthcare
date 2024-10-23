5e0161d845ca5907d115386f9fc3eb6c7f29e457
Julius-0970 <162962316+Julius-0970@users.noreply.github.com>
2024-10-23 22:30:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.get("/sensor-data")
+@router.get("/sensor_data")
 async def get_sensor_data():
     return {"message": "센서 데이터가 여기에 표시됩니다."}
