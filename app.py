fae8362edee488fe49f69ac3b77c710c7f8d7ea7
Julius-0970 <162962316+Julius-0970@users.noreply.github.com>
2024-10-23 22:34:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter
 
+# /api 경로로 묶어서, 한번에 경로 관리를 진행, main.py에 보내서 파일을 나눠서 저장 및 관리가 편함
 router = APIRouter()
 
+
+# 센서 데이터 접근 경로 /api/sensor_data
 @router.get("/sensor_data")
 async def get_sensor_data():
     return {"message": "센서 데이터가 여기에 표시됩니다."}
