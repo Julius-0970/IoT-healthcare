9b141884480b6a1b3ec05c985a5fc03748cfdc0e
Julius-0970 <162962316+Julius-0970@users.noreply.github.com>
2024-10-23 13:12:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import time
 import uvicorn
 from pydantic import BaseModel
 from fastapi.staticfiles import StaticFiles
+from app import router
 
 app = FastAPI()
 app.mount("/", StaticFiles(directory="public", html = True), name="static")
+app.include_router(router)
