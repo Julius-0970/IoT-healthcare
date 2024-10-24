0dfe9c8206a068c553f207edc0d51f92e974e397
Julius-0970 <162962316+Julius-0970@users.noreply.github.com>
2024-10-24 22:37:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from fastapi.staticfiles import StaticFiles
 
 #router로 main이 각 실행파일을 가져오도록 import
 from route import router
-from patient_dispatch import router
+#from patient_dispatch import router
 
 app = FastAPI() # fastapi의 인스턴스를 app이라는 변수에 할당. (쉽게 말하면 fastapi를 다룰 수 있는 리모컨을 app이라는 애한테 줘버린 것.)
 app.include_router(router, prefix="")  # API 루트 경로 설정
