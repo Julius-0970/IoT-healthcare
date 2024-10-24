04040d7877b9e0bea73a0db774c37c52841250b5
Julius-0970 <162962316+Julius-0970@users.noreply.github.com>
2024-10-24 21:24:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ from app import router
 
 app = FastAPI() # fastapi의 인스턴스를 app이라는 변수에 할당. (쉽게 말하면 fastapi를 다룰 수 있는 리모컨을 app이라는 애한테 줘버린 것.)
 app.include_router(router)  # API 루트 경로 설정
-app.mount("/", StaticFiles(directory="public", html = True), name="static") # HTML 파일에 접근(접근 경로는 GIT 내부 디렉토리)_정적파일 접근
+app.mount("/static", StaticFiles(directory="public", html = True), name="static") # HTML 파일에 접근(접근 경로는 GIT 내부 디렉토리)_정적파일 접근
