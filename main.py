0f81a89e2ada320ddce6c94f25465393ce0d23c6
Julius-0970 <162962316+Julius-0970@users.noreply.github.com>
2024-10-24 23:21:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from fastapi import FastAPI, Response
-import time
+from fastapi import FastAPI
 import uvicorn
 from pydantic import BaseModel
 from fastapi.staticfiles import StaticFiles
@@ -9,5 +8,5 @@ from app import router
 #from patient_dispatch import router
 
 app = FastAPI() # fastapi의 인스턴스를 app이라는 변수에 할당. (쉽게 말하면 fastapi를 다룰 수 있는 리모컨을 app이라는 애한테 줘버린 것.)
-app.include_router(router, prefix="")  # API 루트 경로 설정
+app.include_router(router, prefix="/api")  # API 루트 경로 설정
 app.mount("/", StaticFiles(directory="public", html = True), name="static") # HTML 파일에 접근(접근 경로는 GIT 내부 디렉토리)_정적파일 접근
