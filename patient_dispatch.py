0d0f44ff338b13bcbc15fe6e31db703e590a0cbb
Julius-0970 <162962316+Julius-0970@users.noreply.github.com>
2024-10-24 21:12:11 +0900

    Update patient_dispatch.py

--- patient_dispatch.py
+++ patient_dispatch.py
@@ -1,10 +1,10 @@
 # 헬스케어 장치 정보 및 사용자 정보를 받아서 넘겨주는 코드
 
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 from pydantic import BaseModel
 import requests
 
-app = FastAPI()
+router = APIRouter()
 
 # 데이터 형식
 class DeviceData(BaseModel):
@@ -15,7 +15,7 @@ class DeviceData(BaseModel):
 #Back-server url 입력 코드(임시)
 #backend_server_url = "http://{백엔드 서버 IP 주소}"
 
-@app.post("/log")
+@router.post("/log")
 async def receive_ack(data: DeviceData):
     try:
         response = requests.post(backend_server_url, json=data.dict())
@@ -25,6 +25,6 @@ async def receive_ack(data: DeviceData):
         else:
             return {"message" : "너 오늘 밤샘 확정"}
 
-    # 예외처리 코드(서버 작동 중지 방지)
+    # 예외처리 코드(서버 작동 중지 방지)_오류 내용 출력
     except Exception as e:
         return {"message": f"오류 내용 : {str(e)}"}
