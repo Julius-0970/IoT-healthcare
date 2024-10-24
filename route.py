554f788fd73a608ddeb87e600181b612608c0f78
Julius-0970 <162962316+Julius-0970@users.noreply.github.com>
2024-10-24 22:31:35 +0900

    Update route.py

--- route.py
+++ route.py
@@ -6,7 +6,7 @@ router = APIRouter()
 
 
 # 센서 데이터 접근 경로 /api/sensor_data
-@router.get("/sensor_data")
+@router.post("/sensor_data")
  #async를 씀으로 비동기 관련 작업(네트워크 요청, 파일 읽기/쓰기)에 효율적(대기시간 감소 및 자원 활용도 상승) I/O 작업 강추! 동시 작업 가능!
 async def receive_sensor_data(sensor_data: SensorData):  # SensorData값은 센서모델 클래스.
-    return {"message": "센서 데이터가 성공적으로 수신 되었다..."}
+    return {"message": "센서 데이터가 성공적으로 수신 되었다...", "data": sensor_data}
