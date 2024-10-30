31782042b409edd7c44f35c60680c5c119c47429
Julius-0970 <162962316+Julius-0970@users.noreply.github.com>
2024-10-30 23:54:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from body_temp import temp_router
 #from patient_dispatch import router
 
 app = FastAPI() # fastapi의 인스턴스를 app이라는 변수에 할당. (쉽게 말하면 fastapi를 다룰 수 있는 리모컨을 app이라는 애한테 줘버린 것.)
+
 app.include_router(router)  # route.py API 루트 경로 설정
-app.inclue_router(temp_router) # body_temp API 루트 경로 설정
+app.include_router(temp_router) # body_temp API 루트 경로 설정
 app.mount("/", StaticFiles(directory="public", html = True), name="static") # HTML 파일에 접근(접근 경로는 GIT 내부 디렉토리)_정적파일 접근
