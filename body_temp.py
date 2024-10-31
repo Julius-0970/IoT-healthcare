322a036894a297749f917c71c18d392b572e5897
Julius-0970 <162962316+Julius-0970@users.noreply.github.com>
2024-10-31 14:47:21 +0900

    Update body_temp.py

--- body_temp.py
+++ body_temp.py
@@ -14,16 +14,16 @@ async def post_body_temp(data: TemperatureData):
     # 받은 body_temp 값을 큐에 저장
     temperature_data_queue.append(data.body_temp)
     return {"message": "Body temperature received successfully", "body_temp": data.body_temp}
-
+"""
 # 저장된 body_temp 값을 조회하는 엔드포인트 (GET)
 @temp_router.get("/body_temp")
 async def get_body_temp():
     # 저장된 모든 온도 데이터를 반환
     return {"message": "데이터 값이..", "temperature_data": list(temperature_data_queue)}
-"""
+
 
 # WebSocket을 통한 온도 데이터 수신
-@temp_router.websocket("/body_temp")
+@temp_router.websocket("ws/body_temp")
 async def body_temp_websocket(websocket: WebSocket):
     await websocket.accept()
     try:
