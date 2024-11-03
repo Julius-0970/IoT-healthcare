0a02a854ffd2bf1c6255b870f27b4593d38e7788
Julius-0970 <162962316+Julius-0970@users.noreply.github.com>
2024-11-03 20:57:40 +0900

    Update airflow.py

--- airflow.py
+++ airflow.py
@@ -8,7 +8,6 @@ import logging  # 로깅 기능을 사용하기 위한 import
 airflow_router = APIRouter()
 
 # 로깅 설정
-logging.basicConfig(level=logging.INFO)  # INFO 레벨의 로그 메시지를 출력하도록 설정
 logger = logging.getLogger("airflow_logger")  # 현재 모듈의 로거 인스턴스 생성
 
 # AIRFLOW 데이터를 실시간으로 송수신하기 위한 큐(데크) 생성
