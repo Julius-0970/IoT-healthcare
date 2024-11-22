ab8165e5a13c443fa5b6737f088ec328e13f4e99
Julius-0970 <162962316+Julius-0970@users.noreply.github.com>
2024-11-22 09:43:18 +0900

    Update nibp.py

--- nibp.py
+++ nibp.py
@@ -16,7 +16,7 @@ nibp_data = None
 @nibp_router.get("/nibp")
 async def get_nibp():
     if nibp_data is None:
-        return {"message": "No NIBP data available."}  # 데이터가 없을 경우 메시지 반환
+        return {"message": "No NIBP 데이터 없음."}  # 데이터가 없을 경우 메시지 반환
     return {"message": "NIBP data retrieved successfully", "NIBP": nibp_data}
     
 # WebSocket 경로 설정
@@ -26,18 +26,26 @@ async def websocket_nibp(websocket: WebSocket):
     await websocket.accept()
     try:
         while True:
-            # 클라이언트로부터 메시지를 수신
-            message = await websocket.receive_text()
-
             # 데이터 분리 (수축기, 이완기)
             try:
-                systolic, diastolic = map(int, message.split(","))
-                
-                # 로그 출력
-                logger.info(f"Received NIBP data: Systolic={systolic}, Diastolic={diastolic}")
+                #바이너리 데이터 수신
+                data = await websocket.receive_bytes()
+                logger.debug(f"수신된 데이터: {data.hex()}")
+
+                # 데이터 길이 확인
+                if len(data) != 10:
+                    logger.warning(f"잘못된 데이터 크기 수신: {len(data)} bytes. 예상 크기: 10 bytes.")
+                    await websocket.send_text("Invalid packet size. Expected 10 bytes.")
+                    continue
+                    
+                    # 패킷 검증
+                    if data[0] == 0xF7 and data[-1] == 0xFA:
+                        cmd_id = data[1]  # CMD 확인
+                        data_size = data[2]  # DATA SIZE 확인
+                        logger.debug(f"CMD ID: {cmd_id}, DATA SIZE: {data_size}")
                 
                 # 클라이언트에 수신 데이터 전송
-                await websocket.send_text(f"Received NIBP data - Systolic: {systolic}, Diastolic: {diastolic}")
+                await websocket.send_text(f"Received NIBP data - {data.hex()}")
             except ValueError:
                 # 잘못된 데이터 형식 처리
                 logger.warning(f"Invalid data format received: {message}")
