55e9f555f0ce1db4b109e5d30963c04e9ea929e0
Julius-0970 <162962316+Julius-0970@users.noreply.github.com>
2024-11-24 18:24:17 +0900

    Update ecg.py

--- ecg.py
+++ ecg.py
@@ -66,7 +66,7 @@ async def websocket_ecg(websocket: WebSocket):
     """
     await websocket.accept()
     logger.info("WebSocket 연결 수락됨.")
-    username = "example_user"
+    username = "example_user1"
 
     try:
         while True:
