2f068e9fbc0b895a92a1b18d4a863827dab2b4de
Julius-0970 <162962316+Julius-0970@users.noreply.github.com>
2024-11-24 14:32:29 +0900

    Update gsr.py

--- gsr.py
+++ gsr.py
@@ -5,6 +5,10 @@ from logger import get_logger  # 별도의 로깅 설정 가져오기
 # FastAPI 애플리케이션과 연결하는 router 명 지정
 gsr_router = APIRouter()
 
+
+# 로그 설정
+logger = logging.getLogger("gsr_logger")
+
 # GSR 데이터를 실시간으로 송수신하기 위한 큐(데크) 생성
 gsr_data_queue = deque(maxlen=15000)  # 최대 15000개의 파싱된 데이터만 저장
 
