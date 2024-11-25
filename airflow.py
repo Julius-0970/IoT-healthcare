09419b2ffc631ceb9c19fba71025ae917b27f429
Julius-0970 <162962316+Julius-0970@users.noreply.github.com>
2024-11-25 10:51:16 +0900

    Update airflow.py

--- airflow.py
+++ airflow.py
@@ -121,7 +121,7 @@ async def websocket_airflow(websocket: WebSocket):
                     logger.info("WebSocket 연결 종료: 큐가 최대 용량에 도달했습니다.")
                     # WebSocket 연결 종료
                     await websocket.close(code=1000, reason="Queue reached maximum capacity")
-                    await send_data_to_backend(username, "airflow", airflow_data_queue)
+                    await send_data_to_backend(username, "airflow", list(airflow_data_queue))
 
                     # 큐 초기화
                     airflow_data_queue.clear()
