843f57445edecfdee9ec7f7f08204ec69e26fc47
Julius-0970 <162962316+Julius-0970@users.noreply.github.com>
2024-11-25 16:29:30 +0900

    Update ecg.py

--- ecg.py
+++ ecg.py
@@ -80,6 +80,8 @@ async def websocket_ecg(websocket: WebSocket):
     logger.info("ECG 데이터 큐가 초기화되었습니다.")
 
     try:
+        
+        
         # 클라이언트로부터 username 수신
         username = await websocket.receive_text()
         logger.info(f"수신된 사용자 이름: {username}")
@@ -101,7 +103,7 @@ async def websocket_ecg(websocket: WebSocket):
                     logger.info("WebSocket 연결 종료: 큐가 최대 용량에 도달했습니다.")
                     # WebSocket 연결 종료
                     await websocket.close(code=1000, reason="Queue reached maximum capacity")
-                    await send_data_to_backend(username, "ecg", list(ecg_data_queue))
+                    await send_data_to_backend(device_id, username, "ecg", list(ecg_data_queue))
 
                     # 큐 초기화
                     ecg_data_queue.clear()
