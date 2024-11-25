b52aad6a314eedaa2b751222b0be52733fd4a014
Julius-0970 <162962316+Julius-0970@users.noreply.github.com>
2024-11-25 10:52:03 +0900

    Update eog.py

--- eog.py
+++ eog.py
@@ -120,7 +120,7 @@ async def websocket_eog(websocket: WebSocket):
                     logger.info("WebSocket 연결 종료: 큐가 최대 용량에 도달했습니다.")
                     # WebSocket 연결 종료
                     await websocket.close(code=1000, reason="Queue reached maximum capacity")
-                    await send_data_to_backend(username, "eog", eog_data_queue)
+                    await send_data_to_backend(username, "eog", list(eog_data_queue))
 
                     # 큐 초기화
                     eog_data_queue.clear()
