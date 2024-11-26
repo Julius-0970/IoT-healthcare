24db17971ea5f5e6fc6fdbc8678afe26271a7ab6
Julius-0970 <162962316+Julius-0970@users.noreply.github.com>
2024-11-26 17:25:09 +0900

    Update eog.py

--- eog.py
+++ eog.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 from collections import deque
 from logger import get_logger  # 별도의 로깅 설정 가져오기
+from send_data_back import send_data_to_backend # 백엔드로의 전송로직
 
 # FastAPI 애플리케이션과 연결하는 router 명 지정
 eog_router = APIRouter()
